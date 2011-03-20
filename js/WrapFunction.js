9a1b47f948a67616ed29a7b397dd5e1726f88f48
JediMindtrick <bwilhiteforex@yahoo.com>
2011-03-20 09:26:44 -0400

    Files from last commit did not actually get added.

---
+++ js/WrapFunction.js
@@ -0,0 +1,141 @@
+var ChromeObjects =
+['top','window','location','chrome','external','document','$','jQuery','SVGPathSegLinetoVerticalRel','SVGFESpotLightElement',
+'HTMLButtonElement','webkitNotifications','pageYOffset','EntityReference','NodeList','screenY','SVGAnimatedNumber',
+'navigator','MimeTypeArray','offscreenBuffering','sessionStorage','SVGPoint','SVGScriptElement','OverflowEvent',
+'HTMLTableColElement','HTMLOptionElement','HTMLInputElement','SVGFEPointLightElement','SVGPathSegList',
+'SVGImageElement','defaultStatus','SVGMarkerElement','HTMLLinkElement','WebKitCSSTransformValue','MutationEvent',
+'Clipboard','HTMLTableElement','SharedWorker','SVGAElement','SVGAnimatedRect','SVGGElement','toolbar',
+'SVGLinearGradientElement','innerHeight','SVGForeignObjectElement','SVGAnimateElement','applicationCache',
+'SVGFontElement','pageXOffset','SVGFontFaceElement','Element','SVGPathSegCurvetoQuadraticSmoothRel','opener',
+'SVGStopElement','Flags','CSSStyleSheet','StyleSheetList','WebGLShader','Uint32Array','TimeRanges',
+'HTMLHRElement','WebKitPoint','screenLeft','SVGViewElement','CanvasPattern','SVGGradientElement',
+'WebGLActiveInfo','HTMLProgressElement','HTMLDivElement','HashChangeEvent','KeyboardEvent','SVGHKernElement',
+'HTMLTitleElement','HTMLQuoteElement','SVGFEImageElement','DOMTokenList','screenX','WebGLProgram',
+'SVGPathSegMovetoAbs','RangeException','SVGTextPathElement','SVGAnimatedTransformList','HTMLLegendElement',
+'SVGPathSegCurvetoQuadraticAbs','MouseEvent','MediaError','Uint16Array','HTMLObjectElement','HTMLFontElement',
+'SVGFilterElement','WebKitTransitionEvent','MediaList','SVGVKernElement','SVGPaint','SVGFETileElement','Document',
+'XPathException','innerWidth','TextMetrics','personalbar','HTMLHeadElement','SVGFEComponentTransferElement',
+'ProgressEvent','SVGAnimatedPreserveAspectRatio','Node','SVGRectElement','CSSPageRule','SVGLineElement',
+'CharacterData','length','FileError','SVGDocument','MessagePort','ClientRect','Option','SVGDescElement',
+'Notation','WebGLBuffer','StorageEvent','HTMLFieldSetElement','HTMLVideoElement','locationbar',
+'SVGRenderingIntent','SVGPathSegLinetoRel','WebGLTexture','UIEvent','HTMLTableRowElement','SVGEllipseElement',
+'HTMLDListElement','SVGFEFuncRElement','Int32Array','HTMLAllCollection','CSSValue','SVGAnimatedNumberList',
+'HTMLParamElement','SVGElementInstance','HTMLModElement','scrollY','SVGPathSegLinetoHorizontalRel','outerHeight',
+'CSSFontFaceRule','SVGPathSeg','CSSStyleDeclaration','WebSocket','TouchEvent','Rect','StyleSheet',
+'SVGPathSegLinetoHorizontalAbs','SVGColor','ArrayBuffer','SVGComponentTransferFunctionElement','SVGStyleElement',
+'Int16Array','HTMLOutputElement','SVGNumberList','DataView','DeviceOrientationEvent','Blob','SVGFEFloodElement',
+'clientInformation','HTMLStyleElement','HTMLBaseElement','HTMLBRElement','FileReader','SVGFEBlendElement',
+'HTMLHtmlElement','SVGFEConvolveMatrixElement','SVGFEGaussianBlurElement','HTMLTextAreaElement',
+'HTMLBaseFontElement','scrollbars','screen','localStorage','defaultstatus','WebGLRenderbuffer','SVGTextElement',
+'SVGFEOffsetElement','RGBColor','SVGGlyphElement','Float32Array','HTMLCanvasElement','ProcessingInstruction',
+'SVGZoomEvent','HTMLFrameElement','SVGElementInstanceList','SVGFEDisplacementMapElement','frames',
+'SVGPathSegCurvetoCubicSmoothRel','HTMLElement','HTMLSelectElement','Int8Array','SVGFEDistantLightElement',
+'ImageData','SVGFEFuncBElement','HTMLIsIndexElement','HTMLDocument','SVGCircleElement','HTMLCollection',
+'SVGSetElement','SVGFEMergeElement','HTMLDirectoryElement','CSSMediaRule','MessageEvent',
+'SVGFESpecularLightingElement','DOMException','SVGNumber','SVGFontFaceSrcElement','CSSRule','SVGElement',
+'WebKitCSSMatrix','status','SVGMissingGlyphElement','HTMLScriptElement','DOMImplementation','SVGLength',
+'HTMLOptGroupElement','SVGPathSegLinetoVerticalAbs','SVGTextPositioningElement','HTMLKeygenElement','styleMedia',
+'SVGFEFuncGElement','HTMLAreaElement','HTMLFrameSetElement','SVGPathSegCurvetoQuadraticRel','name','self',
+'HTMLIFrameElement','Comment','Event','Storage','XMLSerializer','performance','statusbar','Range',
+'HTMLPreElement','DOMStringList','Image','SVGPathSegCurvetoQuadraticSmoothAbs','SVGRect','parent',
+'SVGFontFaceFormatElement','closed','crypto','SVGAnimateTransformElement','HTMLOListElement','HTMLFormElement',
+'SVGPathSegCurvetoCubicSmoothAbs','DOMParser','SVGPathSegClosePath','console','SVGPathSegArcRel',
+'EventException','SVGAnimatedString','SVGTransformList','SVGPolygonElement','SVGFEMorphologyElement',
+'scrollX','SVGPathSegLinetoAbs','HTMLMediaElement','BlobBuilder','XMLDocument','SVGMaskElement',
+'HTMLHeadingElement','XMLHttpRequest','TextEvent','event','HTMLMeterElement','SVGPathElement','SVGStringList',
+'HTMLAppletElement','devicePixelRatio','FileList','webkitURL','CanvasRenderingContext2D','MessageChannel',
+'WebGLRenderingContext','HTMLMarqueeElement','WebKitCSSKeyframesRule','XSLTProcessor','CSSImportRule',
+'BeforeLoadEvent','PageTransitionEvent','CSSRuleList','SVGAnimatedLengthList','SVGTransform',
+'SVGTextContentElement','HTMLTableSectionElement','SVGRadialGradientElement','HTMLTableCellElement',
+'SVGCursorElement','DocumentFragment','SVGPathSegCurvetoCubicAbs','SVGUseElement','FormData',
+'SVGPreserveAspectRatio','HTMLMapElement','XPathResult','HTMLLIElement','SVGSwitchElement','SVGLengthList',
+'Plugin','HTMLParagraphElement','HTMLBlockquoteElement','SVGPathSegArcAbs','SVGAnimatedBoolean','outerWidth',
+'CSSStyleRule','SVGFontFaceUriElement','Text','HTMLUListElement','WebGLUniformLocation','SVGPointList',
+'CSSPrimitiveValue','HTMLEmbedElement','PluginArray','SVGPathSegCurvetoCubicRel','ClientRectList',
+'SVGMetadataElement','SVGTitleElement','SVGAnimatedAngle','CSSCharsetRule','menubar','SVGAnimateColorElement',
+'SVGMatrix','HTMLBodyElement','SVGSymbolElement','HTMLAudioElement','CDATASection','SVGFEDiffuseLightingElement',
+'SVGFETurbulenceElement','SVGAnimatedEnumeration','WebKitCSSKeyframeRule','Audio','SVGFEMergeNodeElement',
+'history','Entity','SQLException','HTMLTableCaptionElement','DOMStringMap','MimeType','EventSource',
+'SVGException','NamedNodeMap','WebGLFramebuffer','XMLHttpRequestUpload','WebKitAnimationEvent','Uint8Array',
+'SVGAnimatedInteger','HTMLMenuElement','SVGDefsElement','SVGAngle','SVGSVGElement','XPathEvaluator',
+'HTMLImageElement','NodeFilter','SVGAltGlyphElement','SVGClipPathElement','Attr','Counter','SVGPolylineElement',
+'DOMSettableTokenList','SVGPatternElement','SVGFECompositeElement','CSSValueList','XMLHttpRequestException',
+'SVGFEColorMatrixElement','SVGTRefElement','WheelEvent','SVGUnitTypes','HTMLLabelElement','HTMLAnchorElement',
+'SVGFEFuncAElement','CanvasGradient','frameElement','DocumentType','SVGAnimatedLength','File',
+'SVGPathSegMovetoRel','HTMLMetaElement','SVGTSpanElement','Worker','SVGFontFaceNameElement','screenTop',
+'onmouseup','onsubmit','onemptied','postMessage','onpageshow','onstorage','oninvalid','onmousedown','onkeydown',
+'onkeyup','onmessage','onunload','onseeked','ondragend','onended','onstalled','onvolumechange','ondragenter',
+'onpagehide','onmouseover','ondragstart','onabort','oninput','onbeforeunload','onwebkitanimationiteration',
+'onloadstart','onplaying','onhashchange','onprogress','oncanplay','onloadedmetadata','ondurationchange','blur',
+'onseeking','ononline','onwebkittransitionend','onpause','ondragover','close','onloadeddata','onkeypress',
+'onmouseout','onoffline','onload','ondragleave','onpopstate','ondblclick','onsuspend','oncanplaythrough',
+'onreset','onratechange','onerror','onplay','ontimeupdate','onwebkitanimationstart','onwaiting',
+'ondeviceorientation','onresize','focus','ondrag','ondrop','onmousemove','onwebkitanimationend','onscroll',
+'onforminput','onchange','oncontextmenu','onmousewheel','onblur','onsearch','onclick','onformchange','onselect',
+'onfocus','getSelection','print','stop','open','showModalDialog','alert','confirm','prompt','find','scrollBy',
+'scrollTo','scroll','moveBy','moveTo','resizeBy','resizeTo','matchMedia','setTimeout','clearTimeout',
+'setInterval','clearInterval','webkitRequestAnimationFrame','webkitCancelRequestAnimationFrame','atob','btoa',
+'addEventListener','removeEventListener','captureEvents','releaseEvents','getComputedStyle','getMatchedCSSRules',
+'webkitConvertPointFromPageToNode','webkitConvertPointFromNodeToPage','openDatabase','requestFileSystem',
+'dispatchEvent','TEMPORARY','PERSISTENT'];
+
+var getCatchAroundAdvice =
+function(someFunc){
+	var old = someFunc;
+	var new_func = 
+	function(){
+		try{
+			old.apply(this,arguments);
+		}
+		catch(ex){
+			alert('(Custom Logging Here) Logging unhandled exception: ' + ex);
+			throw ex;		
+		}
+	}
+		
+	return new_func;
+};
+
+var interceptAll =
+function(someObj,someInterceptor){
+	var toReturn = true;
+	
+	var funcFilter = function(toCheck){
+		var checkString = toCheck.toString();
+		
+		return someObj.hasOwnProperty(toCheck)
+				&& someObj[k] != undefined
+				&& typeof(someObj[k]) == 'function'
+				&& checkString != 'interceptAll' 
+				&& checkString != 'intercept';
+	};
+
+	if(someObj === window)
+		funcFilter = function(toCheck){
+			var checkString = toCheck.toString();
+			
+			var InChrome = ChromeObjects.indexOf(checkString) != -1;
+			
+			return !InChrome 
+					&& someObj[k] != undefined
+					&& typeof(someObj[k]) == 'function'
+					&& checkString != 'interceptAll' 
+					&& checkString != 'intercept';
+		};
+	
+	for(var k in someObj)
+	{	
+		try {	
+			if (funcFilter(k)
+				&& typeof(someObj[k]) == 'function'
+			) {
+				someObj[k] = someInterceptor(someObj[k]);
+			}
+		}
+		catch (ex){
+			toReturn = false;
+		}
+	}
+	
+	return toReturn;
+};
